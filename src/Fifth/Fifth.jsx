b27eb3ac7b66d32aa6329a227256306a6f21c643
Jeyadev-Muthukumarasamy <jeydev007@gmail.com>
2023-08-29 22:43:08 +0530

    completed upto 5e

---
+++ src/Fifth/Fifth.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Fifth.css'
+const Fifth = () => {
+  return (
+    <div className="container">
+      <div className="fivea">
+      <h4>5a</h4>
+      <p id="tahoma">This is Tahoma font</p>
+      <h4>5b</h4>
+
+      </div>
+      <div className="fiveb">
+      <p id ="fivebfirstline">Biggest deal of the year!</p>
+      <p id ="fivebsecondline">Sales end Tuesday</p>
+
+      </div>
+      <div className="fivec">
+        <h4>5c</h4>
+        <p className="fivecone">HTML CSS Course</p>
+        <p className="fivectwo">Begineer to Pro</p>
+        <p className="fivecthree">In this course,we'll learn the skills you need to become a developer</p>
+        <button className="getstarted">Get Started</button>
+      </div>
+      <div className="fived">
+        <h4>5d</h4>
+        <p className="fivedone">Shopping for your business?</p>
+        <p className="fivedtwo">See how Apple at Work can help.</p>
+        <div className="fivedthree">Learn more &#62;</div>
+      </div>
+      <div className="fivee">
+        <h4>5e</h4>
+        <p className="fiveeone">New</p>
+        <p className="fiveetwo">Macbook Pro</p>
+        <p className="fiveethree">Supercharged for pros</p>
+        <p className="fiveefour">From $1999</p>
+        <button className="buy">Buy</button>
+      </div>
+      
+
+      
+    </div>
+  )
+}
+
+export default Fifth
